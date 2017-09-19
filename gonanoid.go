4148b85a2690075b79286ec9baa5745161bb3b33
Matouš Dzivjak <matoousmaster@gmail.com>
2017-09-19 23:07:01 +0200

    Update gonanoid.go

--- gonanoid.go
+++ gonanoid.go
@@ -1,115 +1,118 @@
-package gonanoid
+package nanoid
 
 import (
 	"crypto/rand"
+	"fmt"
 	"math"
-	"errors"
 )
 
-const (
-	defaultAlphabet = "_~0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" // URL friendly alphabet
-	defaultSize     = 22                                                                 // default size of Nanoid
-)
-
-var (
-	alphabet      = defaultAlphabet                   // alphabet
-	size          = defaultSize                       // id size
-	mask     byte = 1<<computeBits(len(alphabet)) - 1 // mask
-	simple        = true
-	step     int
-)
-
-// Alphabet - sets nanoid alphabet to given string
-func Alphabet(newAlphabet string) error {
-	length := len(newAlphabet)
-	if length < 1 || length > 256 {
-		return errors.New("alphabet length must be between 1 and 256")
-	}
-	// set values
-	alphabet = newAlphabet
-	mask = 1<<computeBits(length) - 1
-	recompute()
-	return nil
+// DefaultsType is the type of the default configuration for Nanoid
+type DefaultsType struct {
+	Alphabet string
+	Size     int
+	MaskSize int
 }
 
-// Size - sets size of generated nanoid
-func Size(newSize int) error {
-	if newSize < 1 {
-		return errors.New("size must be greater than 0")
+// GetDefaults returns the default configuration for Nanoid
+func GetDefaults() *DefaultsType {
+	return &DefaultsType{
+		Alphabet: "_~0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", // len=64
+		Size:     22,
+		MaskSize: 5,
 	}
-	size = newSize
-	recompute()
-	return nil
 }
 
-// Generate - generates nanoid
-func Generate() (string, error) {
-	result := make([]byte, size)
-	// speed up when it is possible
-	if simple {
-		buff := make([]byte, size)
-		if _, err := rand.Read(buff); err != nil {
-			return "", err
-		}
-		for i := 0; i < size; i++ {
-			result[i] = alphabet[buff[i]&mask]
-		}
-		return string(result), nil
+var defaults = GetDefaults()
+
+func initMasks(params ...int) []uint {
+	var size int
+	if len(params) == 0 {
+		size = defaults.MaskSize
+	} else {
+		size = params[0]
 	}
-	buff := make([]byte, step)
-	for u := 0; ; {
-		if _, err := rand.Read(buff); err != nil {
+	/*
+		https://github.com/ai/nanoid/blob/d6ad3412147fa4c2b0d404841ade245a00c2009f/format.js#L1
+		As per 'var masks = [15, 31, 63, 127, 255]'
+
+		The next block initializes an array of size elements, from 2^4-1 to 2^(3 + size)-1
+	*/
+	masks := make([]uint, size)
+	for i := 0; i < size; i++ {
+		shift := 3 + i
+		masks[i] = (2 << uint(shift)) - 1
+	}
+	return masks
+}
+
+/*
+https://github.com/ai/nanoid/blob/d6ad3412147fa4c2b0d404841ade245a00c2009f/format.js#L29-L31
+var mask = masks.find(function (i) {
+	return i >= alphabet.length - 1
+})
+*/
+func getMask(alphabet string, masks []uint) int {
+	for i := 0; i < len(masks); i++ {
+		curr := int(masks[i])
+		if curr >= len(alphabet)-1 {
+			return curr
+		}
+	}
+	return 0
+}
+
+// Random generates cryptographically strong pseudo-random data.
+// The size argument is a number indicating the number of bytes to generate.
+func Random(size int) ([]byte, error) {
+	var randomBytes = make([]byte, size)
+	_, err := rand.Read(randomBytes)
+	return randomBytes, err
+}
+
+// Generate is a low-level function to change alphabet and ID size.
+func Generate(alphabet string, size int) (string, error) {
+	masks := initMasks(size)
+	mask := getMask(alphabet, masks)
+	ceilArg := 1.6 * float64(mask*size) / float64(len(alphabet))
+	step := int(math.Ceil(ceilArg))
+
+	id := make([]byte, size)
+	bytes := make([]byte, step)
+	for j := 0; ; {
+		_, err := rand.Read(bytes)
+		if err != nil {
 			return "", err
 		}
 		for i := 0; i < step; i++ {
-			b := buff[i] & mask
-			if b < byte(len(alphabet)) {
-				result[u] = alphabet[b]
-				u++
-				if u == size {
-					return string(result), nil
+			currByte := bytes[i] & byte(mask)
+			if currByte < byte(len(alphabet)) {
+				id[j] = alphabet[currByte]
+				j++
+				if j == size {
+					fmt.Println("id", id)
+					return string(id[:size]), nil
 				}
 			}
 		}
 	}
 }
 
-// helpers
-
-// find out if number n is power of 2
-func isPowerOfTwo(n int) bool {
-	if n == 0 {
-		return false
-	}
-	for n != 1 {
-		if n%2 != 0 {
-			return false
-		}
-		n = n / 2
-	}
-	return true
-}
-
-// compute bits needed to represent index in alphabet of given size
-func computeBits(size int) (bits uint) {
-	for size--; size != 0; size >>= 1 {
-		bits++
-	}
-	return
-}
-
-// recompute random bytes array length if needed
-func recompute() {
-	// check simple algo availability
-	if isPowerOfTwo(len(alphabet)) {
-		simple = true
+// Nanoid generates secure URL-friendly unique ID.
+func Nanoid(param ...int) (string, error) {
+	var size int
+	if len(param) == 0 {
+		size = defaults.Size
 	} else {
-		simple = false
-		step = computeStep(mask, size, len(alphabet))
+		size = param[0]
 	}
-}
-
-// compute step
-func computeStep(mask byte, size, length int) int {
-	return int(math.Ceil(1.6 * float64(mask) * float64(size) / float64(length)))
+	bytes := make([]byte, size)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err
+	}
+	id := make([]byte, size)
+	for i := 0; i < size; i++ {
+		id[i] = defaults.Alphabet[bytes[i]&63]
+	}
+	return string(id[:size]), nil
 }
